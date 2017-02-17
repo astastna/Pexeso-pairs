cbfd552aa3ffc5c254c4712889185e587ee69745
Aneta Šťastná <aneta.stastna@email.cz>
2017-02-17 17:47:56 +0100

    Minor changes to image scaling. Commit before greater changes.

--- JPexesoCard.java
+++ JPexesoCard.java
@@ -1,6 +1,10 @@
 package pexeso;
 
+import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
@@ -23,10 +27,10 @@ public class JPexesoCard extends JButton{
 	public JPexesoCard(ImageIcon img, int nr, PexesoContainer c, int id){
 		System.out.println("Adding back icon to card nr "+((Integer) nr).toString());
 		this.butt = new JButton(c.backPicture);
-		ImageIcon newBackIcon = transformImageIcon2(c, c.backPicture);
+		ImageIcon newBackIcon = transformImageIcon2(c, butt, c.backPicture);
 		butt.setIcon(newBackIcon);
 		//butt.setText(((Integer) nr).toString());
-		this.front = transformImageIcon2(c, img);
+		this.front = transformImageIcon2(c, butt, img);
 		c.frontPictureTuples[nr][id] = front;
 		this.tupleNr = nr;
 		this.tupleFound = false;
@@ -62,6 +66,7 @@ public class JPexesoCard extends JButton{
 	private void transformImageIcon(PexesoContainer cont2){
 		
 		System.out.println("Transforming an image icon in transformImageIcon.");
+		// finding out the actual size of the grid cell
 		int imgWidth = cont2.getSize().width / cont2.columns;
 		int imgHeight = cont2.getSize().height / cont2.rows;	
 		
@@ -70,37 +75,49 @@ public class JPexesoCard extends JButton{
 		
 		//scale the image with respect to the ratio
 		Image resizedIcon;
-		if (imgHeight > imgWidth) {
-			//resizedIcon = originalIcon.getScaledInstance(-1, imgHeight, java.awt.Image.SCALE_REPLICATE);
-			resizedIcon = originalIcon.getScaledInstance(imgWidth, -1, java.awt.Image.SCALE_SMOOTH);
+		/*if (imgHeight > imgWidth) {
+			resizedIcon = originalIcon.getScaledInstance(-1, imgHeight, java.awt.Image.SCALE_REPLICATE);
+			//resizedIcon = originalIcon.getScaledInstance(imgWidth, -1, java.awt.Image.SCALE_SMOOTH);
 		}
 		else {
-			//resizedIcon = originalIcon.getScaledInstance(imgWidth, -1, java.awt.Image.SCALE_REPLICATE);
-			resizedIcon = originalIcon.getScaledInstance(-1, imgHeight, java.awt.Image.SCALE_SMOOTH);
-		}
+			resizedIcon = originalIcon.getScaledInstance(imgWidth, -1, java.awt.Image.SCALE_REPLICATE);
+			//resizedIcon = originalIcon.getScaledInstance(-1, imgHeight, java.awt.Image.SCALE_SMOOTH);
+		}*/
+		
+		//only for testing
+		resizedIcon = originalIcon.getScaledInstance(imgWidth, imgHeight, java.awt.Image.SCALE_SMOOTH);
 		
 		//replace the image
 		ImageIcon newIcon = new ImageIcon( resizedIcon ); //substitute the old one by the scaled one
 		this.getButton().setIcon(newIcon);
 	}
 	
-	public ImageIcon transformImageIcon2(PexesoContainer cont, ImageIcon orig){
+	public ImageIcon transformImageIcon2(PexesoContainer cont, JButton butt, ImageIcon orig){
 		
 		System.out.println("Transforming an image icon in transformImageIcon2.");
 		int imgWidth = cont.getSize().width / cont.columns;
 		int imgHeight = cont.getSize().height / cont.rows;	
 		
+		/*Dimension d = new Dimension(imgWidth, imgHeight);
+		Rectangle r = new Rectangle(d);
+		butt.add(r);*/
+		
 		// get image from icon
 		Image originalIcon = orig.getImage(); 
 		
+		
 		//scale the image with respect to the ratio
 		Image resizedIcon;// = originalIcon.getScaledInstance(imgWidth, imgHeight, java.awt.Image.SCALE_REPLICATE);
-		if (imgHeight > imgWidth) {
+		/*if (imgHeight > imgWidth) {
 			resizedIcon = originalIcon.getScaledInstance(-1, imgHeight, java.awt.Image.SCALE_REPLICATE); 
 		}
 		else {
 			resizedIcon = originalIcon.getScaledInstance(imgWidth, -1, java.awt.Image.SCALE_REPLICATE);
-		}
+		}*/
+		
+		//only for testing
+		resizedIcon = originalIcon.getScaledInstance(imgWidth, imgHeight, java.awt.Image.SCALE_SMOOTH);
+				
 		
 		return new ImageIcon( resizedIcon ); //substitute the old one by the scaled one
 		
